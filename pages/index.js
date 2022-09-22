74b606cfeb29ad51a3f0cc07e590b8e39edd5990
Puvichandra <45979027+Puvichandra@users.noreply.github.com>
2022-09-23 02:12:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,7 @@ import nftabi from "../contract/abi/nft.json"
 export default function Home(props) {
 
 
- <Head>
-        <title>The Chihuahua Club</title>
-        <meta name="description" content="10,000 The Chihuahua Club NFTs" />
-        <link rel="icon" href="/favicon.ico" />
-  </Head>
+
 
 // <Head>
 // <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Press+Start+2P&display=swap" rel="stylesheet"/>
@@ -125,7 +121,14 @@ const getTokenData= async ()=>{
 
 
 
-  return    <div className='bg-slate-600  font-Press font-bold -top-44'> 
+  return  ( 
+ <>
+  <Head>
+        <title>The Chihuahua Club</title>
+        <meta name="description" content="10,000 The Chihuahua Club NFTs" />
+        <link rel="icon" href="/favicon.ico" />
+  </Head>
+ <div className='bg-slate-600  font-Press font-bold -top-44'> 
   
     <MintComp  connectwallet={props.caddres} appprovider={props.cprovider} totMinted={totalMinted}
       IsPresaleStarted={IsPresaleStarted} IsPublicsaleStarted={IsPublicsaleStarted}
@@ -134,5 +137,6 @@ const getTokenData= async ()=>{
     />
   
     </div>
-
+ </>
+)
 }
